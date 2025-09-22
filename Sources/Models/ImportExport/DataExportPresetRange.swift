e9e5c8bba55adb2a045478a6be5c4481b7e23c40
David Thorn <david.thorn221278@googlemail.com>
2025-09-22 18:22:00 +0200

    Added Import Export Models

---
+++ Sources/Models/ImportExport/DataExportPresetRange.swift
@@ -0,0 +1,28 @@
+//
+//  DataExportPresetRange.swift
+//  FeedingTimerModels
+//
+//  Created by David Thorn on 22.09.25.
+//
+
+import Foundation
+
+public enum DataExportPresetRange: String, CaseIterable, Identifiable, Codable {
+    case last24Hours
+    case last3Days
+    case last7Days
+    case last30Days
+    case all
+    
+    public var id: String { rawValue }
+    
+    public var title: String {
+        switch self {
+        case .all: return "All"
+        case .last24Hours: return "Past 24 Hours"
+        case .last3Days:   return "Past 3 Days"
+        case .last7Days:   return "Past 7 Days"
+        case .last30Days:  return "Past 30 Days"
+        }
+    }
+}
---
+++ Sources/Models/ImportExport/ImportData.swift
@@ -0,0 +1,17 @@
+//
+//  ImportData.swift
+//  FeedingTimerModels
+//
+//  Created by David Thorn on 22.09.25.
+//
+
+import Foundation
+
+public struct ImportData: Codable {
+    public let feeds: [FeedingLogEntry]
+    public let transferType: DataExportPresetRange
+    public init(feeds: [FeedingLogEntry], transferType: DataExportPresetRange) {
+        self.feeds = feeds
+        self.transferType = transferType
+    }
+}
