30ac597cf8388a7b4557c8bd8977464bac413d79
David Thorn <david.thorn221278@googlemail.com>
2025-09-24 13:24:07 +0200

    Added more Models

---
+++ Sources/Models/FeedsPerDay/FeedsPerDayPeriodOption.swift
@@ -0,0 +1,38 @@
+//
+//  FeedsPerDayPeriodOption.swift
+//  FeedingTimerModels
+//
+//  Created by David Thorn on 24.09.25.
+//
+
+import Foundation
+
+public enum FeedsPerDayPeriodOption: String, CaseIterable, Codable, Hashable, Identifiable {
+    
+    private static let key = "FeedsPerDay.period"
+    
+    case last24h
+    case last3d
+    case last7d
+    case last14d
+    case last21d
+
+    public var id: String { rawValue }
+    
+    public var window: (daysBack: Int, rollingHoursBack: Int?) {
+        switch self {
+        case .last24h:  return (1, 24)
+        case .last3d:   return (3, nil)
+        case .last7d:   return (7, nil)
+        case .last14d:  return (14, nil)
+        case .last21d:  return (21, nil)
+        }
+    }
+    
+    public static func load() -> FeedsPerDayPeriodOption {
+        if let raw = UserDefaults.standard.string(forKey: key), let p = FeedsPerDayPeriodOption(rawValue: raw) { return p }
+        return .last7d
+    }
+    
+    public static func persist(_ p: FeedsPerDayPeriodOption) { UserDefaults.standard.set(p.rawValue, forKey: key) }
+}
---
+++ Sources/Statistics/Models/HistoryWindow.swift
@@ -0,0 +1,29 @@
+//
+//  HistoryWindow.swift
+//  FeedingTimerModels
+//
+//  Created by David Thorn on 24.09.25.
+//
+
+import Foundation
+
+public enum HistoryWindow: Int, CaseIterable, Hashable, Identifiable {
+    private static let key = "TimeSpentCompact.daysBack"
+    
+    case last3d = 3
+    case last7d = 7
+    case last14d = 14
+    case last21d = 21
+
+    public var id: Int { rawValue }
+
+    public static func load() -> HistoryWindow {
+        let d = UserDefaults.standard
+        let v = d.integer(forKey: key)
+        return HistoryWindow(rawValue: v) ?? .last7d
+    }
+    
+    public static func persist(_ v: HistoryWindow) {
+        UserDefaults.standard.set(v.rawValue, forKey: key)
+    }
+}
